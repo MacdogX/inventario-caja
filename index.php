96922e3dfc28d7f141537c8e9b8041d80ddaabb0
Miguel angel <soymmiguelr@gmail.com>
2023-05-10 16:33:51 -0500

    Se realiza mvc con login

--- index.php
+++ index.php
@@ -4,9 +4,75 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+
     
-</body>
+    <title>Login</title>
+</head>
+    <body>
+        <header>
+
+        </header>
+        <?php include 'controller/librery/librery.php';?>
+        <main>
+
+        <?php  include 'view/nav/nav.php'; ?>
+
+
+        </main>
+   
+
+        <div class="w-full max-w-xs">
+    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" method="post" action="login.php">
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+        Correo
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+    </div>
+    <div class="mb-6">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+        Contraseña
+      </label>
+      <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+      <p class="text-red-500 text-xs italic">Please choose a password.</p>
+    </div>
+    <div class="flex items-center justify-between">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+      <input type="submit" value="Iniciar sesión">
+      </button>
+      <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+        Forgot Password?
+      </a>
+    </div>
+  </form>
+  <p class="text-center text-gray-500 text-xs">
+    &copy;2020 Acme Corp. All rights reserved.
+  </p>
+</div>
+
+
+
+        <h1>Login</h1>
+  <form method="post" action="login.php">
+    <label for="correo">Correo:</label>
+    <input type="email" id="correo" name="correo" required>
+
+    <br><br>
+
+    <label for="contrasena">Contraseña:</label>
+    <input type="password" id="contrasena" name="contrasena" required>
+
+    <br><br>
+
+   
+  </form>
+
+  <p><a href="recuperar_contrasena.php">¿Olvidaste tu contraseña?</a></p>
+
+
+
+
+        <footer>
+        </footer>
+    </body>
 </html>
