5ff502ee199431d006c8bad2e6e5f40ed5cb0886
Miguel angel <soymmiguelr@gmail.com>
2023-06-26 16:10:59 -0500

    Se carga modulo de estadisticas, trae info de data

--- model/traerdatos.php
+++ model/traerdatos.php
@@ -1,4 +1,5 @@
 <?php 
+use PhpOffice\PhpSpreadsheet\Calculation\Functions;
 
 require_once '../controller/librery/database.php';
 class datosproductos{
@@ -50,6 +51,49 @@ class datosproductos{
         }
     }
 
+}
+
+
+
+
+
+class VentasStatistics {
+    private $id;
+    private $connection;
+
+    public function __construct($connection) {
+        $this->connection = $connection;
+    }
+
+    public function obtenerVentasPorNombre() {
+        try {
+            // Consulta SQL para obtener los datos de ventas por nombre
+            $pdo = $this->connection->conexion();
+            $sql = "SELECT nombre, SUM(precio) AS ganancia_total FROM ventaproducto GROUP BY nombre ORDER BY ganancia_total DESC limit 10";
+            $stmt = $pdo->prepare($sql);
+            
+            // Ejecutar la consulta
+            $stmt->execute();
+            
+            // Obtener los resultados de la consulta
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            // Crear arrays para almacenar los datos de nombres y ganancias
+            $nombres = array();
+            $ganancias = array();
+
+            // Recorrer los resultados de la consulta y almacenar los datos en los arrays
+            foreach ($result as $row) {
+                $nombres[] = $row['nombre'];
+                $ganancias[] = $row['ganancia_total'];
+            }
+
+            return array('nombres' => $nombres, 'ganancias' => $ganancias);
+        } catch(PDOException $e) {
+            // Manejo de errores en caso de que ocurra una excepción PDO
+            echo "Error en la consulta: " . $e->getMessage();
+        }
+    }
 }
 
 ?>
--- view/estadistica.php
+++ view/estadistica.php
@@ -4,107 +4,113 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php include '../controller/librery/librery.php';?>
+    <?php include '../controller/librery/librery.php';
+    require_once '../controller/librery/database.php';
+    
+    ?>
     <title>Document</title>
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/fixedheader/3.2.4/js/dataTables.fixedHeader.min.js"></script>
-<script src="https://cdn.datatables.net/responsive/2.3.0/js/dataTables.responsive.min.js"></script>
-
-<link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
-<link rel="stylesheet" href="https://cdn.datatables.net/fixedheader/3.2.4/css/fixedHeader.dataTables.min.css"0>
-<link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.3.0/css/responsive.dataTables.min.css">
+    <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/fixedheader/3.2.4/js/dataTables.fixedHeader.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.3.0/js/dataTables.responsive.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/fixedheader/3.2.4/css/fixedHeader.dataTables.min.css"0>
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.3.0/css/responsive.dataTables.min.css">
+    <style>
+        @media only screen and (max-width: 600px) {
+            #chartContainer {
+                padding-bottom: 100% !important;
+            }
+        }
+    </style>
 </head>
 <body>
-
     <?php include '../view/nav/nav.php'; ?>
-
     <div class="container mx-auto">
         <div class="grid grid-cols-3 gap-4">
             <div class="col-span-3 md:col-span-1 bg-gray-800 p-4 flex flex-col items-center">
                 <!-- Modal toggle -->
                 <h2 class="text-white mb-4">Modulo de estadistica</h2>
             </div>
+            
             <div class="col-span-3 md:col-span-2 bg-white-800 p-4 flex flex-col items-center">
                 <div class="grid grid-cols-2 gap-4">
                     <div class="col-span-1 bg-red-300">
                         <h2 class="text-white mb-4 "></h2>
                         <div class="container mx-auto">
-
                             <div class="max-w-md mx-auto bg-white p-8 border border-gray-300 mt-8 rounded">
                                 <h2 class="text-2xl font-bold mb-4">Venta realizada Hoy</h2>
-                                <form action="" method="POST">
-                                    <button type="submit"
-                                        class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">Enviar</button>
+                                <form action="../model/traerdatos.php" method="POST" class="max-w-sm mx-auto">
+                                    <div class="mb-4">
+                                        <label for="inicio" class="block mb-2">Inicio</label>
+                                        <input type="date" id="inicio" name="inicio" class="w-full border border-gray-300 rounded px-4 py-2">
+                                    </div>
+                                    <div class="mb-4">
+                                        <label for="final" class="block mb-2">Final</label>
+                                        <input type="date" id="final" name="final" class="w-full border border-gray-300 rounded px-4 py-2">
+                                    </div>
+                                    <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">Enviar</button>
                                 </form>
                             </div>
                         </div>
                     </div>
 
                     <div class="col-span-2">
-                        <h2 class="text-white mb-4">prueba 2</h2>
+                        <h2 class="text-white mb-4">Indicador de ganancias diarias</h2>
+
 
                         <?php
-                        // Conexión a la base de datos MySQL
-                        $servername = "localhost";
-                        $username = "root";
-                        $password = "";
-                        $dbname = "principal";
-
-                        $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-                        // Consulta SQL para obtener los datos de ventas
-                        $query = "SELECT  nombre, 
-                                          precio,
-                                          nombre   
-                                  FROM ventasproducto
-                                  
-                                  ";
-                        $result = mysqli_query($conn, $query);
-
-                        // Crear arrays para almacenar los datos de productos y cantidades
-                        $productos = array();
-                        $cantidades = array();
-
-                        // Recorrer los resultados de la consulta y almacenar los datos en los arrays
-                        while ($row = mysqli_fetch_assoc($result)) {
-                            $productos[] = $row['name_producto'];
-                            $cantidades[] = $row['value_producto'];
-                        }
-
-                        // Cerrar la conexión a la base de datos
-                        mysqli_close($conn);
+                                include '../model/traerdatos.php';
+                                $ventasStats = new VentasStatistics($connection);
+                                // Obtener las ventas por nombre utilizando el método obtenerVentasPorNombre()
+                                $ventasPorNombre = $ventasStats->obtenerVentasPorNombre();
+                                // Acceder a los resultados obtenidos
+                                $nombres = $ventasPorNombre['nombres'];
+                                $ganancias = $ventasPorNombre['ganancias'];
                         ?>
+                        <table class="table-auto w-full">
+                                    <thead>
+                                        <tr>
+                                            <th class="px-4 py-2">Nombre del producto</th>
+                                            <th class="px-4 py-2">Ganancia</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php foreach ($nombres as $key => $nombre): ?>
+                                            <tr>
+                                                <td class="px-4 py-2"><?php echo $nombre; ?></td>
+                                                <td class="px-4 py-2"><?php echo number_format($ganancias[$key], 0, ',', '.'); ?></td>
+                                            </tr>
+                                        <?php endforeach; ?>
+                                    </tbody>
+                                </table>
 
                         <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-                        <div style="position: relative; width: 100%; height: 0; padding-bottom: 56.25%;">
-                            <canvas id="lineChart" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></canvas>
+                        <div id="chartContainer" style="position: relative; width: 100%; height: 0; padding-bottom: 56.25%;">
+                            <canvas id="barChart" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></canvas>
                         </div>
+
 
                         <script>
                             // Obtener los datos de PHP y convertirlos a formato JavaScript
-                            var productos = <?php echo json_encode($productos); ?>;
-                            var cantidades = <?php echo json_encode($cantidades); ?>;
+                            var nombres = <?php echo json_encode($nombres); ?>;
+                            var ganancias = <?php echo json_encode($ganancias); ?>;
 
                             // Crear el contexto del gráfico
-                            var ctx = document.getElementById('lineChart').getContext('2d');
+                            var ctx = document.getElementById('barChart').getContext('2d');
 
-                            // Crear el gráfico de líneas utilizando Chart.js
+                            // Crear el gráfico de barras verticales utilizando Chart.js
                             var chart = new Chart(ctx, {
-                                type: 'line',
+                                type: 'bar',
                                 data: {
-                                    labels: productos,
+                                    labels: nombres,
                                     datasets: [{
-                                        label: 'Cantidad',
-                                        data: cantidades,
-                                        backgroundColor: 'rgba(0, 123, 255, 0.5)', // Color del área debajo de la línea
-                                        borderColor: 'rgba(0, 123, 255, 1)', // Color de la línea
-                                        borderWidth: 1, // Ancho de la línea
-                                        pointBackgroundColor: 'rgba(0, 123, 255, 1)', // Color de los puntos
-                                        pointRadius: 4, // Tamaño de los puntos
-                                        pointHoverRadius: 6, // Tamaño de los puntos al pasar el cursor
-                                        borderDash: [5, 5] // Patrón del límite
+                                        label: 'Ganancias',
+                                        data: ganancias,
+                                        backgroundColor: 'rgba(0, 123, 255, 0.5)', // Color de las barras
+                                        borderColor: 'rgba(0, 123, 255, 1)', // Color del borde de las barras
+                                        borderWidth: 1 // Ancho del borde de las barras
                                     }]
                                 },
                                 options: {
@@ -117,10 +123,13 @@
                                 }
                             });
                         </script>
+
+
+
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
 </body>
+</html>
